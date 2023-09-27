ab8082d3a5822a2d33b9c10b4d59e877ff689443
Madhusudhankn24 <madhusudhankn24@gmail.com>
2023-09-27 12:56:30 +0530

    Alerts Changes

--- src/main/java/ArmorcodepageObjects/AlertspageObjects.java
+++ src/main/java/ArmorcodepageObjects/AlertspageObjects.java
@@ -12,7 +12,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AlertspageObjects {
-
+/*
+hhhh
+*/
 	private WebDriver driver;
 	private WebDriverWait wait;
 	private JavascriptExecutor Js;
